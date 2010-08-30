a4316311d372e6bbf92e9214852286de0286d286
DarkTerror <DarkTerror@LaPosadaDelOcio.com>
2010-08-30 22:03:08 +0200

    '- CF'
    Signed-off-by: DarkTerror <DarkTerror@LaPosadaDelOcio.com>

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -3469,9 +3469,12 @@ void Unit::_UpdateSpells( uint32 time )
                     {
                         if (!aura->GetAuraDuration())
                         {
+                            bool last = aura->IsLastAuraOnHolder();
                             RemoveSingleAuraFromSpellAuraHolder(holder, aura->GetEffIndex(), AURA_REMOVE_BY_EXPIRE);
                             removedAura = true;
-                            break;
+
+                            if (last)
+                                break;
                         }
                     }
                 }
