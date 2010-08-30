956dc4b308ed897a04c1ccc867303e30007f8ba3
DarkTerror <DarkTerror@LaPosadaDelOcio.com>
2010-08-30 22:03:07 +0200

    '- CF'
    Signed-off-by: DarkTerror <DarkTerror@LaPosadaDelOcio.com>

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -8569,7 +8569,10 @@ float Unit::ApplyTotalThreatModifier(float threat, SpellSchoolMask schoolMask)
 
 void Unit::AddThreat(Unit* pVictim, float threat /*= 0.0f*/, bool crit /*= false*/, SpellSchoolMask schoolMask /*= SPELL_SCHOOL_MASK_NONE*/, SpellEntry const *threatSpell /*= NULL*/)
 {
-    // Only mobs can manage threat lists
+    if(!pVictim || !pVictim->isAlive())
+        return;
+
+	// Only mobs can manage threat lists
     if(CanHaveThreatList())
         m_ThreatManager.addThreat(pVictim, threat, crit, schoolMask, threatSpell);
 }
