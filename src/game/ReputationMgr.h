1de8900810e1c27093fe93edf3ed23658e172199
DarkTerror <DarkTerror@LaPosadaDelOcio.com>
2010-08-30 22:02:41 +0200

    '- Reparada la ganancia de reputacion con la Vanguardia de la Alianza y la Expedicion de la Horda'
    Signed-off-by: DarkTerror <DarkTerror@LaPosadaDelOcio.com>

--- src/game/ReputationMgr.h
+++ src/game/ReputationMgr.h
@@ -33,7 +33,8 @@ enum FactionFlags
     FACTION_FLAG_PEACE_FORCED       = 0x10,                 // always overwrite FACTION_FLAG_AT_WAR, used for prevent war with own team factions
     FACTION_FLAG_INACTIVE           = 0x20,                 // player controlled, state stored in characters.data ( CMSG_SET_FACTION_INACTIVE )
     FACTION_FLAG_RIVAL              = 0x40,                 // flag for the two competing outland factions
-    FACTION_FLAG_TEAM_REPUTATION    = 0x80                  // faction has own reputation standing despite teaming up sub-factions; spillover from subfactions will go this instead of other subfactions
+    FACTION_FLAG_TEAM_REPUTATION    = 0x80,                 // faction has own reputation standing despite teaming up sub-factions; spillover from subfactions will go this instead of other subfactions
+    FACTION_FLAG_SPECIAL            = 0x160
 };
 
 typedef uint32 RepListID;
