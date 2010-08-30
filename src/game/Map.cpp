352ff7a5cafdac548075fa4788723f4cde4bb5b0
DarkTerror <DarkTerror@LaPosadaDelOcio.com>
2010-08-30 22:02:52 +0200

    '- CF'
    Signed-off-by: DarkTerror <DarkTerror@LaPosadaDelOcio.com>

--- src/game/Map.cpp
+++ src/game/Map.cpp
@@ -1330,7 +1330,9 @@ bool InstanceMap::Add(Player *player)
                         GetInstanceSave()->GetMapId(), GetInstanceSave()->GetInstanceId(),
                         GetInstanceSave()->GetDifficulty(), GetInstanceSave()->GetPlayerCount(),
                         GetInstanceSave()->GetGroupCount(), GetInstanceSave()->CanReset());
-                    MANGOS_ASSERT(false);
+                    //MANGOS_ASSERT(false);
+					          player->RepopAtGraveyard();
+					          return false;
                 }
             }
             else
@@ -1383,7 +1385,9 @@ bool InstanceMap::Add(Player *player)
                                 sLog.outError("GroupBind save players: %d, group count: %d", groupBind->save->GetPlayerCount(), groupBind->save->GetGroupCount());
                             else
                                 sLog.outError("GroupBind save NULL");
-                            MANGOS_ASSERT(false);
+                            //MANGOS_ASSERT(false);
+														player->TeleportToHomebind();
+														return false;
                         }
                         // if the group/leader is permanently bound to the instance
                         // players also become permanently bound when they enter
@@ -1403,7 +1407,9 @@ bool InstanceMap::Add(Player *player)
                         player->BindToInstance(GetInstanceSave(), false);
                     else
                         // cannot jump to a different instance without resetting it
-                        MANGOS_ASSERT(playerBind->save == GetInstanceSave());
+                        //MANGOS_ASSERT(playerBind->save == GetInstanceSave());
+												player->RepopAtGraveyard();
+												return false;
                 }
             }
         }
