9f961469395e08aff1454aa2c2d235b6409a6849
DarkTerror <DarkTerror@LaPosadaDelOcio.com>
2010-09-01 17:58:37 +0200

    '- AURA_284 - LINKED'
    Signed-off-by: DarkTerror <DarkTerror@LaPosadaDelOcio.com>

--- src/game/SpellAuras.h
+++ src/game/SpellAuras.h
@@ -364,6 +364,8 @@ class MANGOS_DLL_SPEC Aura
         void HandlePhase(bool Apply, bool Real);
         void HandleModTargetArmorPct(bool Apply, bool Real);
         void HandleAuraModAllCritChance(bool Apply, bool Real);
+        void HandleAllowOnlyAbility(bool Apply, bool Real);
+        void HandleAuraLinked(bool Apply, bool Real);
         void HandleAuraOpenStable(bool apply, bool Real);
 
         virtual ~Aura();
