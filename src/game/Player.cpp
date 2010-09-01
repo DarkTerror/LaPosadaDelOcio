9cf170e32426aa46854e8ffcbc631d34138fe58b
DarkTerror <DarkTerror@LaPosadaDelOcio.com>
2010-09-01 17:57:32 +0200

    '- Permite ganar logros mediante Script'
    Signed-off-by: DarkTerror <DarkTerror@LaPosadaDelOcio.com>

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -22553,6 +22553,16 @@ Object* Player::GetObjectByTypeMask(ObjectGuid guid, TypeMask typemask)
     return NULL;
 }
 
+void Player::CompletedAchievement(AchievementEntry const* entry)
+{
+  GetAchievementMgr().CompletedAchievement(entry);
+}
+
+void Player::CompletedAchievement(uint32 uiAchievementID)
+{
+  GetAchievementMgr().CompletedAchievement(sAchievementStore.LookupEntry(uiAchievementID));
+}
+
 void Player::SetRestType( RestType n_r_type, uint32 areaTriggerId /*= 0*/)
 {
     rest_type = n_r_type;
