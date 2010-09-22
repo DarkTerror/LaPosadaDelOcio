804aed19527afcc44d29e76dc2278238ac31ac24
DarkTerror <DarkTerror@LaPosadaDelOcio.com>
2010-09-22 22:43:33 +0200

    '- DK 56815 "Golpe con runa" NO TESTEADO'
    Signed-off-by: DarkTerror <DarkTerror@LaPosadaDelOcio.com>

--- src/game/UnitAuraProcHandler.cpp
+++ src/game/UnitAuraProcHandler.cpp
@@ -3295,6 +3295,13 @@ SpellAuraProcResult Unit::HandleProcTriggerSpellAuraProc(Unit *pVictim, uint32 d
                         return SPELL_AURA_PROC_FAILED;
                 }
             }
+            // Rune Strike
+            else if (auraSpellInfo->Id == 56816)
+            {
+                if( Aura * pAura = this->GetAura(56816, EFFECT_INDEX_0))
+                    pAura->GetHolder()->SendFakeAuraUpdate(56817, false);
+                    return SPELL_AURA_PROC_OK;
+            }
             // Blade Barrier
             else if (auraSpellInfo->SpellIconID == 85)
             {
