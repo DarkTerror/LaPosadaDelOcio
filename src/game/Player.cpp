bdf1c4029ed0dd038fe96d9aa494adf69bd37451
DarkTerror <DarkTerror@LaPosadaDelOcio.com>
2010-10-01 22:23:24 +0200

    '- AntiSpam del spell 58600 "Area de vuelo restringida" en Dalaran'
    Signed-off-by: DarkTerror <DarkTerror@LaPosadaDelOcio.com>

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -13654,6 +13654,8 @@ void Player::AddQuest( Quest const *pQuest, Object *questGiver )
         for(SpellAreaForAreaMap::const_iterator itr = saBounds.first; itr != saBounds.second; ++itr)
             if(itr->second->autocast && itr->second->IsFitToRequirements(this,zone,area))
                 if (!HasAura(itr->second->spellId, EFFECT_INDEX_0) )
+
+                if(itr->second->spellId != 58600 || (HasAuraType(SPELL_AURA_MOD_FLIGHT_SPEED_MOUNTED) || HasAuraType(SPELL_AURA_FLY) || IsTaxiFlying()))
                     CastSpell(this,itr->second->spellId,true);
     }
 
