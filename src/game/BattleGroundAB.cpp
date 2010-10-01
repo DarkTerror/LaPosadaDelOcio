611ef60e0ef6f141d2ceac64ddc88a153e168ae5
DarkTerror <DarkTerror@LaPosadaDelOcio.com>
2010-10-01 21:20:48 +0200

    '- Permite ganar XP en BG'
    Signed-off-by: DarkTerror <DarkTerror@LaPosadaDelOcio.com>

--- src/game/BattleGroundAB.cpp
+++ src/game/BattleGroundAB.cpp
@@ -114,6 +114,7 @@ void BattleGroundAB::Update(uint32 diff)
                 m_TeamScores[team] += BG_AB_TickPoints[points];
                 m_HonorScoreTics[team] += BG_AB_TickPoints[points];
                 m_ReputationScoreTics[team] += BG_AB_TickPoints[points];
+                m_ExperiencesTicks[team] += BG_AB_TickPoints[points];
                 if (m_ReputationScoreTics[team] >= m_ReputationTics)
                 {
                     (team == BG_TEAM_ALLIANCE) ? RewardReputationToTeam(509, 10, ALLIANCE) : RewardReputationToTeam(510, 10, HORDE);
@@ -132,6 +133,11 @@ void BattleGroundAB::Update(uint32 diff)
                         SendMessageToAll(LANG_BG_AB_H_NEAR_VICTORY, CHAT_MSG_BG_SYSTEM_NEUTRAL);
                     PlaySoundToAll(BG_AB_SOUND_NEAR_VICTORY);
                     m_IsInformedNearVictory = true;
+                }
+                if (m_ExperiencesTicks[team] >= BG_AB_ExperiencesTicks)
+                {
+                    RewardXpToTeam(0, 0.8, team);
+                    m_ExperiencesTicks[team] -= BG_AB_ExperiencesTicks;
                 }
 
                 if (m_TeamScores[team] > BG_AB_MAX_TEAM_SCORE)
@@ -462,6 +468,7 @@ void BattleGroundAB::Reset()
         m_TeamScores[i]          = 0;
         m_lastTick[i]            = 0;
         m_HonorScoreTics[i]      = 0;
+        m_ExperiencesTicks[i]    = 0;
         m_ReputationScoreTics[i] = 0;
         m_TeamScores500Disadvantage[i] = false;
     }
@@ -488,12 +495,20 @@ void BattleGroundAB::EndBattleGround(uint32 winner)
 {
     //win reward
     if (winner == ALLIANCE)
+	{
         RewardHonorToTeam(GetBonusHonorFromKill(1), ALLIANCE);
+        RewardXpToTeam(0, 0.8, winner);
+	}
     if (winner == HORDE)
+	{
         RewardHonorToTeam(GetBonusHonorFromKill(1), HORDE);
+        RewardXpToTeam(0, 0.8, winner);
+	}
     //complete map_end rewards (even if no team wins)
     RewardHonorToTeam(GetBonusHonorFromKill(1), HORDE);
     RewardHonorToTeam(GetBonusHonorFromKill(1), ALLIANCE);
+    RewardXpToTeam(0, 0.8, HORDE);
+    RewardXpToTeam(0, 0.8, ALLIANCE);
 
     BattleGround::EndBattleGround(winner);
 }
