5dd5f5ec6266c61557090d5ca6136d88ff919b57
DarkTerror <DarkTerror@LaPosadaDelOcio.com>
2010-11-22 19:28:31 +0100

    '- CF'
    Signed-off-by: DarkTerror <DarkTerror@LaPosadaDelOcio.com>

--- src/game/Object.h
+++ src/game/Object.h
@@ -156,7 +156,10 @@ class MANGOS_DLL_SPEC Object
 
         const uint64& GetUInt64Value( uint16 index ) const
         {
-            MANGOS_ASSERT( index + 1 < m_valuesCount || PrintIndexError( index , false) );
+            //MANGOS_ASSERT( index + 1 < m_valuesCount || PrintIndexError( index , false) );
+            if (!m_uint32Values)
+                return GetUInt64Value(0);
+
             return *((uint64*)&(m_uint32Values[ index ]));
         }
 
