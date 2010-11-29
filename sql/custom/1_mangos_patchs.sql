99b42e0756cfe3fc35ee999930b216961d922d61
DarkTerror <DarkTerror@LaPosadaDelOcio.com>
2010-11-29 05:21:25 +0100

    '- Anyadida opcion de mostrar el id del equipo de arena'
    Signed-off-by: DarkTerror <DarkTerror@LaPosadaDelOcio.com>

--- sql/custom/1_mangos_patchs.sql
+++ sql/custom/1_mangos_patchs.sql
@@ -58,3 +58,7 @@ VALUES
 ('sban ip','5','Sintaxis: .sban ip $Ip $tiempodebaneo $razon
 Banea la IP.
 $tiempodeban: Colocar un valor negativo conlleva a baneo permanente, de otra manera usar una línea de tiempo como la siguiente "4d20h3s".');
+DELETE FROM `mangos_string` WHERE `entry`='718';
+INSERT INTO `mangos_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`)
+VALUES
+('718','|cffff0000[Arena Queue Announcer]:|r All Arenas -- Joined : %ux%u : %u -- Tean %u|r',NULL,NULL,NULL,NULL,NULL,'|cffff0000[Anuncio de Cola de Arena]:|r Todas las Arenas -- Unido : %ux%u : %u -- Equipo %u|r','|cffff0000[Anuncio de Cola de Arena]:|r Todas las Arenas -- Unido : %ux%u : %u -- Equipo %u|r',NULL);
---
+++ sql/custom/mangos_announce_team_arena.sql
@@ -0,0 +1,4 @@
+DELETE FROM `mangos_string` WHERE `entry`='718';
+INSERT INTO `mangos_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`)
+VALUES
+('718','|cffff0000[Arena Queue Announcer]:|r All Arenas -- Joined : %ux%u : %u -- Tean %u|r',NULL,NULL,NULL,NULL,NULL,'|cffff0000[Anuncio de Cola de Arena]:|r Todas las Arenas -- Unido : %ux%u : %u -- Equipo %u|r','|cffff0000[Anuncio de Cola de Arena]:|r Todas las Arenas -- Unido : %ux%u : %u -- Equipo %u|r',NULL);
